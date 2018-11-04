21ad0bccd48c2866d455ca6f4414322bc2d3ba04
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-11-04 15:49:03 +0100

    feat(chat): add component representing a single message

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublishMessageComponent } from './components/publish-message/publish-message.component';
+import { MessageCardComponent } from './components/message-card/message-card.component';
 
 @NgModule({
-  declarations: [PublishMessageComponent],
+  declarations: [PublishMessageComponent, MessageCardComponent],
   imports: [CommonModule]
 })
 export class ChatModule {}
