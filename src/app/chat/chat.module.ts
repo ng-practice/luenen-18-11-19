baa96203159a72c52910a0843cb3d66139c6ab4a
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-11-04 15:48:21 +0100

    feat(chat): add component to enter and share a message

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PublishMessageComponent } from './components/publish-message/publish-message.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PublishMessageComponent],
   imports: [CommonModule]
 })
 export class ChatModule {}
---
+++ src/app/chat/components/publish-message/publish-message.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publish-message',
+  templateUrl: './publish-message.component.html',
+  styleUrls: ['./publish-message.component.scss']
+})
+export class PublishMessageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
