51ccd9e1902847020dc6beda85722e5fdb68f7b2
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-11-18 19:00:02 +0100

    cypress: add fist test

--- cypress/integration/simple.spec.ts
+++ cypress/integration/simple.spec.ts
@@ -1,5 +1,11 @@
+import { MockedChatServer } from '../support/chat-server.mock';
+
 describe('When I try to run cypress with typescript', () => {
-  beforeEach(() => cy.visit('http://localhost:4200'));
+  let server: MockedChatServer;
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+    server = new MockedChatServer();
+  });
 
   it('should work', () => {
     cy.get('h1').contains('Chat');
