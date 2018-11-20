f36acd1991e45c57b883e84f699b626c9ab07e21
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-11-20 13:22:15 +0100

    store: introduce store-freeze

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,10 +1,5 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
 import { environment } from '../../environments/environment';
 
 export interface State {}
@@ -12,5 +7,5 @@ export interface State {}
 export const reducers: ActionReducerMap<State> = {};
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
-  ? []
+  ? [storeFreeze]
   : [];
