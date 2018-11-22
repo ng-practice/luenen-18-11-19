fee12374af4169e4b6487ffdeafbec744dfaceb8
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-11-22 13:57:59 +0100

    setup tslint html report

---
+++ tools/tslint-report.js
@@ -0,0 +1,13 @@
+const tslintHtmlReport = require('tslint-html-report');
+const config = {
+  tslint: './tslint.json', // path to tslint.json
+  srcFiles: './src/**/*.ts', // files to lint
+  outDir: './reports/tslint-html-report', // output folder to write the report to
+  html: 'tslint-report.html', // name of the html report generated
+  // exclude: ['src/**/*.ts'], // Files/patterns to exclude
+  breakOnError: true, // Should it throw an error in tslint errors are found
+  typeCheck: true, // enable type checking. requires tsconfig.json
+  tsconfig: './tsconfig.json'
+};
+
+tslintHtmlReport(config, () => {});
